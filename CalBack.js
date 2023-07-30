fd50195f98941f2751620702175e6f334dc90dee
Swagat <swagatswaroop@gmail.com>
2023-07-30 19:19:55 +0530

    Added Some More Features using Chat-GPT and GitCo

--- CalBack.js
+++ CalBack.js
@@ -1,7 +1,3 @@
-// function cli(){
-
-// }
-
 var a = document.getElementsByClassName('Button');
 var b = document.getElementById('display');
 
@@ -15,21 +11,35 @@ for(var i = 0;i < a.length;i++){
         if(value == '+'){
             operator = '+';
             operand1 = parseFloat(b.textContent);
-
+            b.innerText = '';
         }
         else if(value == '-'){
-            
+            operator = '-';
+            operand1 = parseFloat(b.textContent);
         }
         else if(value == '*'){
-            
+            operator = '*';
+            operand1 = parseFloat(b.textContent);
         }
         else if(value == '/'){
-            
+            operator = '/';
+            operand1 = parseFloat(b.textContent);
         }
         else if(value == '='){
             operand2 = parseFloat(b.textContent);
-            //use eval to get result
-            //Show result in display
+            var a = eval('operand1' + operator + 'operand2');
+            b.innerText = a;
+            operand1 = a;
+            operand2 = 0;
+        }
+        else if(value == 'AC'){
+            b.innerText = a;
+            operator = 0;
+            operand1 = 0;
+            operand2 = 0;
+        }
+        else if(value == 'C'){
+            b.innerText = b.innerText.slice(0,-1);
         }
         else{
             b.innerText += value;
